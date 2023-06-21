e7540693e11f429699391ad993957fba9a770e74
常雲翔 <103975914+supercyx3@users.noreply.github.com>
2023-06-21 20:37:22 +0800

    delete description

--- src/store/modules/user/helper.ts
+++ src/store/modules/user/helper.ts
@@ -17,7 +17,7 @@ export function defaultSetting(): UserState {
     userInfo: {
       avatar: 'https://raw.githubusercontent.com/Chanzhaoyu/chatgpt-web/main/src/assets/avatar.jpg',
       name: 'CYX',
-      description: 'Star on <a href="https://github.com/supercyx3/chatgpt-web" class="text-blue-500" target="_blank" >GitHub</a>',
+      description: '',
     },
   }
 }
