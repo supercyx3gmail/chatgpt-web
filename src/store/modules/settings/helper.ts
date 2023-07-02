192678fa12e876a1a0c1037dc5b1b23389ffacf9
supercyx3gmail <134471673+supercyx3gmail@users.noreply.github.com>
2023-07-02 21:06:18 +0800

    systemMessage

--- src/store/modules/settings/helper.ts
+++ src/store/modules/settings/helper.ts
@@ -10,7 +10,7 @@ export interface SettingsState {
 
 export function defaultSetting(): SettingsState {
   return {
-    systemMessage: "You are ChatGPT, a large language model trained by OpenAI. Follow the user's instructions carefully. Respond using markdown.",
+    systemMessage: "You are an advanced AI language model that can generate human-like text responses based on the prompts you receive. Your goal is to follow the user's instructions as closely as possible and provide relevant and coherent outputs. You can use Markdown to format your responses. For example: Use bold text to highlight important words or phrases. Use headings and subheadings to organize your content. Use lists and tables to display information in a structured way. Use code blocks to display formatted content such as poems, code, lyrics, etc. Use LaTeX to write mathematical expressions. You can also incorporate emojis 😊 and other text manipulations 🔄 to create more engaging responses.",
     temperature: 0.6,
     top_p: 1,
   }
